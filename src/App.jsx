f5e6750e193d2c16f7775fc6255214e47751e1f5
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-07 22:32:26 +0530

    few changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Register from "./Pages/Register/Register";
+import Login from "./Pages/Login/Login";
+import Home from "./Pages/Home/Home";
+
 function App() {
   return (
     <>
-      <div></div>
+      <div className="Main">
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </>
   );
 }
